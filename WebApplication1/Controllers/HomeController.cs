ad6302f5629906b6b613b93b23f50ae5c84f2810
NarminSH <nshivakhanova@gmail.com>
2022-09-22 19:55:29 +0400

    second commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,67 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.DataAccess;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        MyDbContext context = new MyDbContext();
 
         public IActionResult Index()
         {
-            var products = new List<Product> {
-            new Product
-            { Name = "Fresh Organic Kiwi",
-              StartPrice = 10,
-              Price = 90,
-              Rate = 5,
-              ProductPhotos = new List<ProductPhoto>{new ProductPhoto
-              {
-                Url = "/images/products/product-image-2-1.jpg"
-                }
-              },
-            },new Product
-            { Name = "Dried Mango",
-              StartPrice = 10,
-              Price = 70,
-              Rate = 5,
-              ProductPhotos = new List<ProductPhoto>{new ProductPhoto
-              {
-                Url = "/images/products/product-image-1-2.jpg"
-                }
-              },
-            }, new Product
-            { Name = "Dried Banana",
-              StartPrice = 60,
-              Price = 80,
-              Rate = 4,
-              ProductPhotos = new List<ProductPhoto>{new ProductPhoto
-              {
-                Url = "/images/products/product-image-3-1.jpg"
-                }
-              },
-            }, new Product
-            { Name = "Crunchy Chips",
-              StartPrice = 50,
-              Price = 90,
-              Rate = 4,
-              ProductPhotos = new List<ProductPhoto>{new ProductPhoto
-              {
-                Url = "/images/products/product-image-4-1.jpg"
-                }
-              },
-            }};
+
+            var products = context.Products.Include("ProductPhotos").ToList();
+            Console.WriteLine(products);
             return View(products);
         }
 
--- WebApplication1/DataAccess/MyDbContext.cs
+++ WebApplication1/DataAccess/MyDbContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+Console.WriteLine("here");
 namespace WebApplication1.DataAccess
 {
-    public class MyDbContext:DbContext
+    public class MyDbContext : DbContext
     {
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductPhoto> Photos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=localhost;database=Product;Trusted_Connection=True");
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
